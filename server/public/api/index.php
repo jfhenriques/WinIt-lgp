e0cfa35d4d34d923f53063e4d7de5005c4a1912f
Server <postmaster@lgptlantic.fe.up.pt>
2013-04-10 17:54:10 +0100

    changes in tree struct

---
+++ server/public/api/index.php
@@ -0,0 +1,20 @@
+<?php
+
+	
+	DEFINE('MEASURE_TIME', true);
+	$st = 0;
+	
+	if( MEASURE_TIME ) $st = microtime(true);
+	/*define('INCLUSION_PATH', '/billsystem/');
+	require('../views/header.html.php');
+	require('../views/home/home.html.php');
+	require('../views/footer.html.php');*/
+	
+	require('../../bootstrap.php');
+
+	if( MEASURE_TIME )
+	{
+		$end = 1000 * (microtime(true) - $st);	
+		print("<br>Time: $end ms\n");
+	}
+?>
