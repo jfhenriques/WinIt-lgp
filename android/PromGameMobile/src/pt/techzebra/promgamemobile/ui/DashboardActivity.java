74dfa8aa0a36cf6b43be6782cda97649fe5bd811
Renato Rodrigues <hi@renatorodrigues.me>
2013-04-13 18:49:44 +0100

    Added a menu to the action bar on the dashboard screen.

--- android/PromGameMobile/src/pt/techzebra/promgamemobile/ui/DashboardActivity.java
+++ android/PromGameMobile/src/pt/techzebra/promgamemobile/ui/DashboardActivity.java
@@ -6,30 +6,66 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
 
 public class DashboardActivity extends SherlockActivity {
     private static final String TAG = "DashboardActivity";
-    
+
     private SharedPreferences preferences_;
-    
+
+    private ActionBar action_bar_;
+
     @Override
     protected void onCreate(Bundle saved_instance_state) {
         super.onCreate(saved_instance_state);
-        
-        preferences_ = getSharedPreferences(Constants.USER_PREFERENCES, Context.MODE_PRIVATE);
-        
-        boolean logged_in = preferences_.getBoolean(Constants.PREF_LOGGED_IN, false);
+
+        action_bar_ = getSupportActionBar();
+        action_bar_.setTitle(R.string.dashboard);
+
+        preferences_ = getSharedPreferences(Constants.USER_PREFERENCES,
+                Context.MODE_PRIVATE);
+
+        boolean logged_in = preferences_.getBoolean(Constants.PREF_LOGGED_IN,
+                false);
         if (!logged_in) {
             Intent intent = new Intent(this, AuthenticationActivity.class);
             startActivity(intent);
             finish();
             return;
         }
-        
+
         setContentView(R.layout.dashboard_activity);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getSupportMenuInflater();
+        inflater.inflate(R.menu.menu_dashboard, menu);
+        return true;
+    }
     
-   
+    public void handleRoomSelection(View view) {
+        String msg = null;
+        switch (view.getId()) {
+        case R.id.single_player_layout:
+            msg = "Single Player";
+            break;
+        case R.id.cooperative_layout:
+            msg = "Cooperative";
+            break;
+        case R.id.competitive_layout:
+            msg = "Competitive";
+            break;
+        case R.id.trading_layout:
+            msg = "Trading";
+            break;
+        }
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
 }
