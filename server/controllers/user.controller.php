3d39abff943f82c9ddd09a8b8dbb0f48eb956a68
Ana Ferreira <anamarg83@gmail.com>
2013-04-15 16:54:50 +0100

    Get user information with level and points fields 0

--- server/controllers/user.controller.php
+++ server/controllers/user.controller.php
@@ -33,8 +33,13 @@
 				$porta_andar = $user->getPortaAndar();
 				$t_fb = $user->getTokenFacebook();
 				$t_tw = $user->getTokenTwitter();
+				$level = 0;
+				$points = 0;
 				
-				$this->respond->renderJSON( array('name' => $name, 'email' => $email, 'cp4' => $cp4, 'cp3' => $cp3, 'door' => $porta_andar, 'token_fb' => $t_fb, 'token_tw' => $t_tw, 'uid' => $userId) );
+				
+				$this->respond->renderJSON( array('name' => $name, 'email' => $email, 'cp4' => $cp4, 'cp3' => $cp3,
+													'door' => $porta_andar, 'token_fb' => $t_fb, 'token_tw' => $t_tw,
+													'uid' => $userId, 'level' => $level, 'points' => $points) );
 				
 				
 			}				
@@ -42,7 +47,34 @@
 	
 		public function create() {
 			
-			//$_REQUEST();
+			$nome = valid_request('name');
+			$email = valid_request('email');
+			$cp4 = valid_request('cp4');
+			$cp3 = valid_request('cp3');
+			$door = valid_request('portaAndar');
+			$t_fb = valid_request('token_fb');
+			$t_tw = valid_request('token_tw');
+			$password = valid_request('password');
+			
+			if( is_null($nome) || is_null($email) || is_null($cp4) || is_null($cp3) || is_null($door) || is_null($t_fb) || is_null($t_tw) || is_null($password)) {
+				$render_code = 1;
+			} else {
+				$this->respond->renderJSON( array('name' => $name, 'email' => $email, 'cp4' => $cp4, 'cp3' => $cp3, 'door' => $porta_andar, 'token_fb' => $t_fb, 'token_tw' => $t_tw, 'uid' => $userId, 'password' => $passaword) );
+			}
 		}
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 ?>
