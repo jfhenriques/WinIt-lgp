3a33901bd0fe537cebac3c215ce384c0ced0e318
TiagoMota <neozflux@gmail.com>
2013-04-19 12:48:15 +0100

    Added BadgesActivity and TagsActivity. Noticed some error on fetchingUserInformation and login in: the auth token was not what I expected

---
+++ android/PromGameMobile/src/pt/techzebra/promgamemobile/ui/BadgesActivity.java
@@ -0,0 +1,13 @@
+package pt.techzebra.promgamemobile.ui;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class BadgesActivity extends SherlockActivity {
+
+	@Override
+	protected void onCreate(Bundle saved_instance_state) {
+		super.onCreate(saved_instance_state);
+	}
+}
