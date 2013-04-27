c6b30421e5c07372f6db20ae240642bcc4b8a610
Renato Rodrigues <hi@renatorodrigues.me>
2013-04-27 17:32:04 +0100

    Refactored NetworkUtilities. Added methods for post, get, put and delete and another new three methods to validate a server response, to get its message and, the last one, to get its content if a valid response.

--- android/PromGameMobile/src/pt/techzebra/promgamemobile/client/NetworkUtilities.java
+++ android/PromGameMobile/src/pt/techzebra/promgamemobile/client/NetworkUtilities.java
@@ -2,10 +2,8 @@ package pt.techzebra.promgamemobile.client;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.TimeZone;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
@@ -13,19 +11,21 @@ import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
-import org.apache.http.auth.AuthenticationException;
-import org.apache.http.client.ClientProtocolException;
+import org.apache.http.ParseException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.conn.params.ConnManagerParams;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -33,7 +33,6 @@ import pt.techzebra.promgamemobile.Constants;
 import pt.techzebra.promgamemobile.PromGame;
 import pt.techzebra.promgamemobile.ui.AuthenticationActivity;
 
-import android.accounts.Account;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Handler;
@@ -49,23 +48,19 @@ public class NetworkUtilities {
     public static final String PARAM_PASSWORD = "password";
     public static final String PARAM_NAME = "name";
     public static final String PARAM_BIRTHDAY = "date";
-    public static final String PARAM_CP4 = "cp4";
-    public static final String PARAM_CP3 = "cp3";
-    public static final String PARAM_DOOR = "portaAndar";
-    public static final String PARAM_FLOOR = "floor";
+    public static final String PARAM_PC4 = "cp4";
+    public static final String PARAM_PC3 = "cp3";
+    public static final String PARAM_HOUSE_NUMBER = "portaAndar";
     public static final String PARAM_UPDATED = "timestamp";
     public static final String PARAM_AUTH_TOKEN = "token";
     
     public static final String USER_AGENT = "AuthenticationService/1.0";
     public static final int REGISTRATION_TIMEOUT = 30 * 1000; // ms
     public static final String HOST = "lgptlantic.fe.up.pt";
-    // TODO: change to the correct URL
     public static final String BASE_URL = "http://lgptlantic.fe.up.pt/api";
 
     public static final String AUTH_URI = BASE_URL + "/session.json";
-    public static final String FETCH_USER_URI = BASE_URL + "/user.json";
-    //TODO o de baixo nao serve para nada, em vez de se fazer get ao users, faz-se um post ao FETCH_USER_URI
-    public static final String NEW_USER_URI = FETCH_USER_URI + "/new.json";
+    public static final String USER_URI = BASE_URL + "/user.json";
 
     private static HttpClient http_client_;
 
@@ -104,6 +99,102 @@ public class NetworkUtilities {
         return t;
     }
 
+    private static JSONObject executeRequest(HttpRequest request) {        
+        maybeCreateHttpClient();
+        HttpHost host = new HttpHost(HOST);
+        
+        HttpResponse response;
+        try {
+            response = http_client_.execute(host, request);
+            int status_code = response.getStatusLine().getStatusCode();
+            
+            if (status_code == HttpStatus.SC_OK) {
+                JSONObject json_response = new JSONObject(EntityUtils.toString(response.getEntity()));
+                
+                return json_response;
+            } else {
+                
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        
+        return null;
+    }
+    
+    private static JSONObject simpleRequest(HttpRequestBase request) {
+        return executeRequest(request);
+    }
+    
+    private static JSONObject enclosingRequest(HttpEntityEnclosingRequestBase request, ArrayList<NameValuePair> parameters) {
+        HttpEntity entity = null;
+        try {
+            entity = new UrlEncodedFormEntity(parameters);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        
+        request.addHeader(entity.getContentType());
+        request.setEntity(entity);
+        
+        return executeRequest(request);
+    }
+    
+    private static JSONObject get(String uri) {
+        return simpleRequest(new HttpGet(uri));
+    }
+    
+    private static JSONObject delete(String uri) {
+        return simpleRequest(new HttpDelete(uri));
+    }
+    
+    private static JSONObject post(String uri, ArrayList<NameValuePair> parameters) {
+        return enclosingRequest(new HttpPost(uri), parameters);
+    }
+    
+    private static JSONObject put(String uri, ArrayList<NameValuePair> parameters) {
+        return enclosingRequest(new HttpPut(uri), parameters);
+    }
+    
+    public static boolean validResponse(JSONObject response) {
+        try {
+            return response.getString("s") == "0";
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        
+        return false;
+    }
+    
+    public static String getResponseMessage(JSONObject response) {
+        String message = null;
+        try {
+            message = response.getString("m");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return message;
+    }
+    
+    public static JSONObject getResponseContent(JSONObject response) {
+        JSONObject content = null;
+        
+        if (validResponse(response)) {
+            try {
+                content = response.getJSONObject("r");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        return content;
+    }
+    
     /**
      * Connects to the server, authenticates the provided username and password.
      * 
@@ -120,68 +211,32 @@ public class NetworkUtilities {
      * @throws JSONException 
      */
     public static boolean authenticate(String username, String password,
-            Handler handler, final Context context) throws JSONException {
-        final HttpResponse http_response;
+            Handler handler, final Context context) {
         final ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
         params.add(new BasicNameValuePair(PARAM_EMAIL, username));
         params.add(new BasicNameValuePair(PARAM_PASSWORD, password));
 
-        HttpEntity entity = null;
-        try {
-            entity = new UrlEncodedFormEntity(params);
-        } catch (final UnsupportedEncodingException e) {
-            throw new AssertionError(e);
-        }
-
-        final HttpPost post = new HttpPost(AUTH_URI);
-        post.addHeader(entity.getContentType());
-        post.setEntity(entity);
-        maybeCreateHttpClient();
-
-        try {
-            http_response = http_client_.execute(post);
-            if (http_response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-                if (Log.isLoggable(TAG, Log.VERBOSE)) {
-                    Log.v(TAG, "Successful authentication");
-                    
-                }
-                final String response = EntityUtils.toString(http_response.getEntity());
-                JSONObject response_json = new JSONObject(response);
-                int response_s = response_json.getInt("s");
-                if (response_s != 0) {
-                	sendResultToAuthenticationActivity(false, handler, context);
-                	return false;
-                } else {
-                	JSONObject response_r = response_json.getJSONObject("r");
-                    SharedPreferences.Editor preferences_editor = PromGame.getAppContext().getSharedPreferences(
-                            Constants.USER_PREFERENCES, Context.MODE_PRIVATE).edit();
-                    preferences_editor.putString(Constants.PREF_AUTH_TOKEN, response_r.getString("token"));
-                    preferences_editor.putBoolean(Constants.PREF_LOGGED_IN, true);
-                    preferences_editor.commit();
-                    sendResultToAuthenticationActivity(true, handler, context);
-                    return true;
-                }
-                
-            } else {
-                if (Log.isLoggable(TAG, Log.VERBOSE)) {
-                    Log.v(TAG,
-                            "Error authenticating"
-                                    + http_response.getStatusLine());
-                }
-                sendResultToAuthenticationActivity(false, handler, context);
-                return false;
-            }
-        } catch (IOException e) {
-            if (Log.isLoggable(TAG, Log.VERBOSE)) {
-                Log.v(TAG, "IOException when getting authtoken", e);
-            }
+        JSONObject json_response = post(AUTH_URI, params);
+        
+        JSONObject r = getResponseContent(json_response);
+        if (r == null) {
             sendResultToAuthenticationActivity(false, handler, context);
-            return false;
-        } finally {
-            if (Log.isLoggable(TAG, Log.VERBOSE)) {
-                Log.v(TAG, "getAuthtoken completing");
+        } else {
+            SharedPreferences.Editor preferences_editor = PromGame.getAppContext().getSharedPreferences(
+                    Constants.USER_PREFERENCES, Context.MODE_PRIVATE).edit();
+            try {
+                preferences_editor.putString(Constants.PREF_AUTH_TOKEN, r.getString("token"));
+                preferences_editor.putBoolean(Constants.PREF_LOGGED_IN, true);
+                preferences_editor.commit();
+                sendResultToAuthenticationActivity(true, handler, context);
+                
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
         }
+        
+        return false;
     }
 
     /**
@@ -215,115 +270,61 @@ public class NetworkUtilities {
         final Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                try {
-					authenticate(username, password, handler, context);
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+                authenticate(username, password, handler, context);
             }
         };
 
         return NetworkUtilities.performOnBackgroundThread(runnable);
     }
 
-    public static User fetchUserInformation(String email, String auth_token,
-            Date last_updated) throws ClientProtocolException, IOException,
-            JSONException, AuthenticationException {
+    public static User fetchUserInformation(String auth_token, Date last_updated) {
+        String uri = USER_URI + "?token=" + auth_token;      
+        JSONObject json_response = get(uri);
         
-        String URI = FETCH_USER_URI + "?token=" + auth_token;
-        final HttpGet get = new HttpGet(URI);
-        HttpHost host = new HttpHost(HOST);
-        maybeCreateHttpClient();
-        final HttpResponse http_response = http_client_.execute(host, get);
-        final String response = EntityUtils.toString(http_response.getEntity());       
         User user = null;
 
-        if (http_response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-        	JSONObject response_json = new JSONObject(response);
-        	JSONObject response_r = response_json.getJSONObject("r");
-        	user = new User(response_r.getInt("uid"), response_r.getString("name"), response_r.getString("email"), response_r.getString("level"), response_r.getString("points") );
-        } else {
-            if (http_response.getStatusLine().getStatusCode() == HttpStatus.SC_UNAUTHORIZED) {
-                Log.e(TAG,
-                        "Authentication exception in fetching user information");
-                throw new AuthenticationException();
-            } else {
-                Log.e(TAG, "Server error in fetching user information");
-                throw new IOException();
-            }
-        }
+    	JSONObject r = getResponseContent(json_response);
+        user = User.valueOf(r);
 
         return user;
     }
     
-    //TODO falta adicionar o floor à BD
     private static boolean register(String name, String username, String password,
-            String birthday, String cp4, String cp3, String door, Handler handler,
+            String birthday, String pc4, String pc3, String house_number, Handler handler,
             final Context context) {
-        final HttpResponse resp;
-
         final ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
         params.add(new BasicNameValuePair(PARAM_EMAIL, username));
         params.add(new BasicNameValuePair(PARAM_NAME, name));
         params.add(new BasicNameValuePair(PARAM_PASSWORD, password));
         params.add(new BasicNameValuePair(PARAM_BIRTHDAY, birthday));
-        params.add(new BasicNameValuePair(PARAM_CP4, cp4));
-        params.add(new BasicNameValuePair(PARAM_CP3, cp3));
-        params.add(new BasicNameValuePair(PARAM_DOOR, door));
+        params.add(new BasicNameValuePair(PARAM_PC4, pc4));
+        params.add(new BasicNameValuePair(PARAM_PC3, pc3));
+        params.add(new BasicNameValuePair(PARAM_HOUSE_NUMBER, house_number));
         
-        HttpEntity entity = null;
-        
-        try {
-            entity = new UrlEncodedFormEntity(params);
-        } catch (UnsupportedEncodingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        
-        final HttpPost post = new HttpPost(FETCH_USER_URI);
-        post.addHeader(entity.getContentType());
-        post.setEntity(entity);
-        maybeCreateHttpClient();
-        
-        try {
-            resp = http_client_.execute(post);
-            if(resp.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
-                attemptAuth(username, password, handler, context);
-                Log.v(TAG, "Successful register");
-                return true;
-            }else{
-                //TODO ver JSONArray com os erros que o servidor deteta
-                if(resp.getStatusLine().getStatusCode() == HttpStatus.SC_UNAUTHORIZED){
-                    Log.e(TAG, "Register exception in fetching remote");
-                    return false;
-                }else{
-                    Log.e(TAG, "Server error in fetching remote info" + resp.getStatusLine());
-                    return false;
-                }
-            }
+        JSONObject json_response = post(USER_URI, params);
+        if (validResponse(json_response)) {
+            attemptAuth(username, password, handler, context);
+            Log.v(TAG, "Successful register");
             
-        } catch (IOException e) {
-            Log.v(TAG, "IOException when getting authtoken", e);
+            return true;
+        } else {
+            
             return false;
-        } finally {
-            Log.v(TAG, "getAuthtoken completing");
         }
-        
     }
 
-    //TODO falta o floor porque nao há campo na BD
     public static Thread attemptRegister(final String name, final String email,
-            final String password, final String birthday, final String cp4,
-            final String cp3, final String door, final Handler handler,
+            final String password, final String birthday, final String pc4,
+            final String pc3, final String house_number, final Handler handler,
             final Context context) {
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                register(name, email, password, birthday, cp4, cp3, door, handler,
+                register(name, email, password, birthday, pc4, pc3, house_number, handler,
                         context);
             }
         };
+        
         return NetworkUtilities.performOnBackgroundThread(runnable);
     }
 }
