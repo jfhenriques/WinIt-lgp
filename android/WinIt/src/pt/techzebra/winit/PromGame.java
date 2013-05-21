4a2de479a8344d58e9448074acacc15595d435f7
Renato Rodrigues <hi@renatorodrigues.me>
2013-05-21 04:48:29 +0100

    Added a method to store and retrieve the user authentication token

--- android/WinIt/src/pt/techzebra/winit/PromGame.java
+++ android/WinIt/src/pt/techzebra/winit/PromGame.java
@@ -2,9 +2,12 @@ package pt.techzebra.winit;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.SharedPreferences;
 
 public class PromGame extends Application {
 	private static Context context_;
+	
+	private static String auth_token_ = null;
 	
 	@Override
 	public void onCreate() {
@@ -15,5 +18,14 @@ public class PromGame extends Application {
 	
 	public static Context getAppContext() {
 		return context_;
+	}
+	
+	public static String getAuthToken() {
+	    if (auth_token_ == null) {
+	        SharedPreferences preferences = context_.getSharedPreferences(Constants.USER_PREFERENCES, Context.MODE_PRIVATE);
+	        auth_token_ = preferences.getString(Constants.PREF_AUTH_TOKEN, null);
+	    }
+	    
+	    return auth_token_;
 	}
 }
